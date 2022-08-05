ce70c52d0fa71bfeca15f200e350748a7c34c68d
kartikey <ksaraswat400@gmail.com>
2022-08-05 09:07:17 +0530

    added images

---
+++ js/verify.js
@@ -0,0 +1,13 @@
+function verify() {
+	var token = sessionStorage.getItem("token");
+//	console.log(sessionStorage.getItem("token"));
+//	console.log(token);
+	if (token === null){
+		window.location.replace("./login.html");
+	}
+}
+function logout() {
+      sessionStorage.clear();
+      alert('You are about to Logout');
+      window.location.href='login.html'
+    }
