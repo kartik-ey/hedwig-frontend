73d6f7d1e4a98b9aaaf281ece9007ceb92668755
kartikey <ksaraswat400@gmail.com>
2022-08-09 05:46:31 +0530

    added avis create and profile page

--- js/verify.js
+++ js/verify.js
@@ -1,7 +1,7 @@
 function verify() {
 	var token = sessionStorage.getItem("token");
-//	console.log(sessionStorage.getItem("token"));
-//	console.log(token);
+//console.log(sessionStorage.getItem("token"));
+//console.log(token);
 	if (token === null){
 		window.location.replace("./login.html");
 	}
@@ -11,3 +11,26 @@ function logout() {
       alert('You are about to Logout');
       window.location.href='login.html'
     }
+/*
+function displayTime() {
+  var str = "";
+
+  var currentTime = new Date()
+  var hours = currentTime.getHours()
+  var minutes = currentTime.getMinutes()
+  var seconds = currentTime.getSeconds()
+
+  if (minutes < 10) {
+      minutes = "0" + minutes
+  }
+  if (seconds < 10) {
+      seconds = "0" + seconds
+  }
+  str += hours + ":" + minutes + ":" + seconds + " ";
+  if(hours > 11){
+      str += "PM"
+  } else {
+      str += "AM"
+  }
+  return str;
+}  */
